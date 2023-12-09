1fab724cc6b2992dcc349b1b234fb1507cf1c34e
gcarnab <gcarnab74@gmail.com>
2023-12-09 19:19:37 +0100

    mod

--- GUI/TURTLE/D&D/diego_paesaggio.py
+++ GUI/TURTLE/D&D/diego_paesaggio.py
@@ -12,7 +12,7 @@ turtle.setup(width, height)
 t = turtle.Turtle()
 
 # Imposta la velocità della tartaruga
-t.speed(1)  # 0 è la massima velocità
+t.speed(0)  # 0 è la massima velocità
 
 t.home()
 t.write(t.pos())
@@ -25,7 +25,7 @@ def disegna_erba():
     colore_linea = "green"
     t.color(colore_linea)  # Imposta il colore della linea
 
-    colore_fill = "green"
+    colore_fill = "#00e600"
     # Imposta il colore del riempimento
     t.fillcolor(colore_fill)
 
@@ -51,9 +51,11 @@ def disegna_cielo() :
     t.home()
 
     colore_linea = "blue"
-    t.color(colore_linea)  # Imposta il colore della linea
+    #t.color(colore_linea)  # Imposta il colore della linea
+    t.pencolor(colore_linea)
 
-    colore_fill = "blue"
+
+    colore_fill = "#4d79ff"
     # Imposta il colore del riempimento
     t.fillcolor(colore_fill)
 
@@ -63,42 +65,92 @@ def disegna_cielo() :
     t.left(90)
     t.pendown()
     t.forward(width/2)
-    t.right(90)
-    t.forward(height/4)  
-    t.right(90)
+    t.left(90)
+    t.forward(height - height/4)  
+    t.left(90)
     t.forward(width)
     t.left(90)
-    t.forward(height/2)
-
+    t.forward(height - height/4)  
+    t.left(90)
+    t.forward(width/2)
     t.end_fill()
 
 #funzione che disegna il sole
-def disegna_sole() :
+def disegna_sole(radius,rays_flag) :
     t.penup()  
     t.home()    
-
+    
     colore_linea = "yellow"
-    t.color(colore_linea)  # Imposta il colore della linea
-
+    t.pencolor(colore_linea)  # Imposta il colore della linea
+    
     colore_fill = "yellow"
     # Imposta il colore del riempimento
     t.fillcolor(colore_fill)
+    t.goto(width/4,height/4)
+    t.begin_fill()
+    t.circle(radius)
+    t.end_fill()
+
+    if rays_flag :
+        # Disegna i raggi attorno al cerchio del sole
+        t.penup()
+        t.goto(width/4,height/4 + radius)
+        t.pendown()
+        t.color("yellow")  # Puoi cambiare il colore dei raggi se lo desideri
+        t.pensize(1)
+        for _ in range(36):  # 12 raggi
+            t.forward(radius*1.5)
+            t.backward(radius*1.5)
+            t.left(10)  # Angolo tra i raggi
+
+
+#disegna casa 
+def disegna_casa() :
+    t.penup()  
+    t.home() 
+
+    colore_linea = "#cc6600" #corpo casa 
+    t.pencolor(colore_linea)  # Imposta il colore della linea
+
+    colore_linea = "#cc6600"
+    # Imposta il colore del riempimento
+    t.goto(width/4,-height/4)
+    
+    colore_fill = "#ffbf80"
+    #imposta il colore del riempimento
+    t.fillcolor("#ffbf80")
 
     t.begin_fill()
-
-    t.forward(width/4)
-    t.left(90)
-    t.forward(height)
-
-
+    t.pendown()
+    for _ in range(4):
+        t.forward(100)
+        t.left(90)
     t.end_fill()
+
+
+
+
+
+
+
+
+
+
+
+
 
 #===== DISEGNO FIGURE =====
 
-#disegna_erba()
-disegna_cielo()
-#disegna_sole()
 
+
+disegna_erba()
+disegna_cielo()
+#disegna_sole(50,True)
+disegna_casa()
+
+
+#t.home()
+#t.write(t.pos())
 
 # Chiudi la finestra quando si fa clic sopra di essa
 turtle.done()
