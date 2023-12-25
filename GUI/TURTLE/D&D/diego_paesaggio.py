a13319a7d0eeb4506b4a3a1e035515eb5b9560b3
gcarnab <gcarnab74@gmail.com>
2023-12-25 11:39:49 +0100

    diego from home

--- GUI/TURTLE/D&D/diego_paesaggio.py
+++ GUI/TURTLE/D&D/diego_paesaggio.py
@@ -1,4 +1,5 @@
 import turtle
+import random
 
 #======= SETTINGS =======#
 #VARIABILI GLOBALI
@@ -14,98 +15,59 @@ t = turtle.Turtle()
 # Imposta la velocità della tartaruga
 t.speed(0)  # 0 è la massima velocità
 
-colore_linea = "green"
-t.color(colore_linea)  # Imposta il colore della linea
 
-colore_fill = "green"
-# Imposta il colore del riempimento
-t.fillcolor(colore_fill)
-
-    t.begin_fill()
-    t.right(90)
-    t.forward(height/4)
-    t.pendown()
-    t.left(90)
-    t.forward(width/2)
-    t.right(90)
-    t.forward(height/4)
-    t.right(90)
-    t.forward(width)
-    t.right(90)
-    t.forward(height/4)
-    t.right(90)
-    t.forward(width/2)
-    t.end_fill()
 
 #funzione che disegna il cielo
 def disegna_cielo() :
     t.penup()  
     t.home()
+    colore_linea = "#000000"
+    t.pencolor(colore_linea)  # Imposta il colore della linea
 
-colore_linea = "blue"
-t.color(colore_linea)  # Imposta il colore della linea
+    colore_fill = "#FFFF00"
+    # Imposta il colore del riempimento
+    t.fillcolor(colore_fill)
 
-colore_fill = "blue"
-# Imposta il colore del riempimento
-t.fillcolor(colore_fill)
+    t.begin_fill()
 
-t.begin_fill()
+    t.penup()
+    t.left(90)
+    t.forward(600)
+    t.pendown()
+    t.left(90)
+    t.forward(400)
+    t.left(90)
+    t.forward(600)
+    t.left(90)
+    t.forward(800)
+    t.left(90)
+    t.forward(600)
+    t.left(90)
+    t.forward(400)
 
-t.penup()
-t.left(90)
-t.forward(600)
-t.pendown()
-t.left(90)
-t.forward(400)
-t.left(90)
-t.forward(600)
-t.left(90)
-t.forward(800)
-t.left(90)
-t.forward(600)
-t.left(90)
-t.forward(400)
-
-t.end_fill()
-
-#========================================
-
-colore_linea = "yellow"
-t.color(colore_linea)  # Imposta il colore della linea
-
-colore_fill = "yellow"
-# Imposta il colore del riempimento
-t.fillcolor(colore_fill)
-
-t.begin_fill()
-
-t.penup()
-t.goto(250,350)
-t.pendown()
-t.circle(50)
+    t.end_fill()
 
 
-t.end_fill()
-
-#===== DISEGNO FIGURE =====
-
-#disegna_erba()
-disegna_cielo()
-#disegna_sole()
-
-
-#===== DISEGNO FIGURE =====
+def test(line_color, fill_color) :
+    t.pencolor(line_color)    
+    t.fillcolor(fill_color)
+    t.home()
+    t.write(t.pos())
+    shapes = ["turtle", "arrow", "circle"]
+    selected_shape = random.choice(shapes)
+    #print("selected_shape= ", selected_shape)
+    t.shape(selected_shape)
 
 
 
-disegna_erba()
-disegna_cielo()
-#disegna_sole(50,True)
-disegna_casa()
+#===== MAIN =====
+test("#FFFF00","#FF6600")
+
+#============ CALL FUNCTIONS ===============
+#disegna_cielo()
 
 
-#t.home()
-#t.write(t.pos())
+
 
 # Chiudi la finestra quando si fa clic sopra di essa
 turtle.done()
